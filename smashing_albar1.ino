a01c2641045ee0cc8af22bb8dab96d58fb330b73
Wejdan_AlHarby <108189544+Dooni0harbi@users.noreply.github.com>
2026-07-08 11:13:53 +0300

    change code

--- smashing_albar1.ino
+++ smashing_albar1.ino
@@ -1,29 +1,3 @@
-
-/*
-// C++ code
-//
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(2, OUTPUT);
-
-}
-
-
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);   */
-  
-  //delay(1000); }// Wait for 1000 millisecond(s)
-
-
-
 #include <Servo.h>
 
 Servo servo1;
